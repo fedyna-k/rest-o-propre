e481cc9f6ef99e60215d7bbbcbcde8412d8515ba
ARAB-Ryan <ryan.arab@etu.univ-amu.fr>
2023-05-17 20:47:35 +0200

    Revert "maj react-native from "0.71.7" to "0.71.8""
    This reverts commit 0d024e772cf6570be375c31fc701bb8d12c7853b.

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import MapView from 'react-native-maps';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-      <MapView style={styles.map} />
     </View>
   );
 }
@@ -19,8 +17,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  map: {
-    width: '100%',
-    height: '100%',
-  }
 });
